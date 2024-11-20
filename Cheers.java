df9a63b09a50e5f3622d8956377422cce03b0f80
barbuium <bar.buium@gmail.com>
2024-11-20 22:03:41 +0200

    cheers

--- Cheers.java
+++ Cheers.java
@@ -2,19 +2,18 @@
 public class Cheers {
         public static void main(String[] args) {
             String inputString = args[0].toUpperCase();  
-                   int numCheers = Integer.parseInt(args[1]);   // Convert second argument to integer
+                   int numCheers = Integer.parseInt(args[1]);
                     
                  String specialLetters = "AEFHILMNORSX";
                   for (int i = 0; i < inputString.length(); i++) {
-                    char letter = inputString.charAt(i); // Get the letter
+                    char letter = inputString.charAt(i); 
                     
                   if (specialLetters.indexOf(letter) != -1) {
                    System.out.println("Give me an " + letter + ": " + letter + "!");
                   } else {
                       System.out.println("Give me a " + letter + ": " + letter + "!");
               }
-                }
-                    
+                }    
                     System.out.println("What does that spell?");
                   for (int i = 0; i < numCheers; i++) {
                     System.out.println(inputString + "!!!");
