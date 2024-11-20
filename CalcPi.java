7f1e56e33f6f715f8b822db15062f5ab164ddd2e
barbuium <bar.buium@gmail.com>
2024-11-20 21:36:31 +0200

    calcpi completed

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,21 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+				int numTerms = Integer.parseInt(args[0]);
+				double piApproximation = 0.0;
+		
+				for (int i = 0; i < numTerms; i++) {
+					double denominator = 2 * i + 1;
+		
+					if (i % 2 == 0) {
+						piApproximation += 1 / denominator;
+					} else {
+						piApproximation -= 1 / denominator;
+					}
+				}
+		
+				piApproximation *= 4;
+				System.out.println("pi according to Java: " + Math.PI);
+				System.out.println("pi, approximated: " + piApproximation);		
 	}
 }
