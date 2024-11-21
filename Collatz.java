b33f6b6be735f3d4ee425dadca4878312da39d7a
barbuium <bar.buium@gmail.com>
2024-11-21 14:49:10 +0200

    collatz

--- Collatz.java
+++ Collatz.java
@@ -7,19 +7,21 @@ public class Collatz {
         for (int seed = 1; seed <= N; seed++) {
             if (mode.equals("v")) {
                 int current = seed;
-                int steps = 0;
-                System.out.print(current);
+                int steps = 1;
+                StringBuilder sequence = new StringBuilder();
 
-            while (current != 1) {
+				sequence.append(current);
+
+            	while (current != 1) {
                 if (current % 2 == 0) {
                     current = current / 2;
                 } else if (current % 2 != 0) {
 					current = 3 * current + 1;	
 				}
                 steps++;
-            System.out.print(" " + current);
+				sequence.append(" ").append(current);
         	}
-                System.out.println(" (" + (steps + 1) + ")");
+                System.out.println(sequence + " (" + steps + ")");
 				System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
             }
         }
